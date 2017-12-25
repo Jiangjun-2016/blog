df57d5f6db09b0a131a97a273f6bd518170bd132
fan <295392867@qq.com>
2017-12-25 14:59:34 +0800

    bolg

---
+++ blog/src/main/test/test.java
@@ -0,0 +1,13 @@
+/**
+ * @author fxf
+ * @create 2017-12-18 11:26
+ **/
+
+public class test {
+
+	public static void main(String[] args) {
+		// System.out.printf("");
+		// System.out.printf("");
+	}
+
+}
